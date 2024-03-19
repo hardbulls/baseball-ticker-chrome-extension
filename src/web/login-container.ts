d569449579732939803c4fd2513315835e2a03d1
Arjan Frans <arjan.frans@fusonic.net>
2024-03-19 15:01:26 +0100

    add download button

--- src/web/login-container.ts
+++ src/web/login-container.ts
@@ -1,4 +1,4 @@
-import './login-container.css'
+import "./login-container.css";
 import { setComponent } from "../framework";
 import { LoginComponent } from "./login-component";
 
@@ -13,13 +13,18 @@ export class LoginContainer extends HTMLElement {
         super();
 
         this.innerHTML = this.render();
-        this.style.marginInline = '24px';
+        this.style.marginInline = "24px";
 
         const loginComponent = new LoginComponent(defaultCredentials, async (username, password) => {
             return await handleLogin(username, password);
         });
 
-        (this.querySelector('#login-form-container') as HTMLDivElement).append(loginComponent);
+        (this.querySelector("#login-form-container") as HTMLDivElement).append(loginComponent);
+        const downloadCxtButton = this.querySelector("#download-cxt-button") as HTMLButtonElement;
+
+        downloadCxtButton.addEventListener("click", () => {
+            window.open("./wbsc-ticker-cxt.zip");
+        });
     }
 
     private render(): string {
